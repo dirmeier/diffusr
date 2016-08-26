7bf494e50c943b91441f7d1b750285f762a98171
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-08-26 20:07:33 +0200

    readme

--- R/diffusr-package.R
+++ R/diffusr-package.R
@@ -1,3 +1,12 @@
+#' Package: diffusr
+#'
+#' Network diffusion algorithms in R.
+#'
+#' @author Simon Dirmeier | Computational Biology Group, ETH ZURICH | \email{simon.dirmeier@@bsse.ethz.ch}
+#' @name diffusr-package
+#'
+#' @docType package
+#' @keywords package
 #'
 #' @useDynLib diffusr
 #' @importFrom Rcpp sourceCpp
