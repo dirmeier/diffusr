accc5a1964f175fb858badc546916801dfd20cf2
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-09-26 22:01:44 +0200

    added license

--- R/diffusr-package.R
+++ R/diffusr-package.R
@@ -1,3 +1,22 @@
+# diffusr: network diffusion algorithms in R
+#
+# Copyright (C) 2016 Simon Dirmeier
+#
+# This file is part of diffusr.
+#
+# diffusr is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# diffusr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with diffusr. If not, see <http://www.gnu.org/licenses/>.
+
 #' diffusr
 #'
 #' Network diffusion algorithms in R.
